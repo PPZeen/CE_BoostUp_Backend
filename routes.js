2b80fa5e42f20a6e5fe3b04548c190637f65b43b
PPZeen <88711532+PPZeen@users.noreply.github.com>
2022-06-17 21:43:37 +0700

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const homeController = require('./controller/homeController')
-
-router.get('/', (req,res) => homeController.helloHome(req,res))
-router.get('/name', (req,res) => homeController.getHome(req,res))
-
-module.exports = router
